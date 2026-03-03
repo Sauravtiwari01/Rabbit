7805fc35195c7590443b3824b4639e92b7c736cd
Saurav Tiwari <sauravtiwari010@gmail.com>
2026-03-03 15:49:58 +0530

    Minor UI fix

--- frontend/src/components/products/NewArrivals.jsx
+++ frontend/src/components/products/NewArrivals.jsx
@@ -65,7 +65,7 @@ const NewArrivals = () => {
         }
     }, [newArrivals])
     return (
-        <section className='py-16 px-5'>
+        <section className='py-16 px-5 sm:p-16'>
             <div className='container mx-auto text-center mb-10 relative'>
                 <h2 className='font-bold text-3xl mb-4'>Explore New Arrivals</h2>
                 <p className='text-lg text-gray-800 mb-10 '>Discover the latest styles straight off the runway, freshly added to keep your wardrobe on the cutting edge of fashion.</p>
@@ -81,8 +81,8 @@ const NewArrivals = () => {
             <div ref={scrollRef} className={`container mx-auto no-scrollbar overflow-x-scroll flex space-x-6 relative ${isDragging ? "cursor-grabbing" : "cursor-grab"}`} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUpOrLeave} onMouseLeave={handleMouseUpOrLeave}>
                 {newArrivals.map((product, index) => {
                     return <div key={index} className='min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative'>
-                        <img src={product.images[0]?.url} alt={product.images[0]?.alttext || product.name} className='w-full h-[500px] object-cover rounded-lg' draggable={false} />
-                        <div className='absolute right-0 left-0 bottom-0 p-4 text-white rounded-b-lg bg-opacity-50 backdrop-blur-md'>
+                        <img src={product.images[0]?.url} alt={product.images[0]?.alttext || product.name} className='w-full h-[500px] object-cover border rounded-lg' draggable={false} />
+                        <div className='absolute right-0 left-0 bottom-0 p-4 text-white bg-black rounded-b-lg bg-opacity-50 backdrop-blur-md'>
                             <Link className='block' to={`/product/${product._id}`}>
                                 <h4 className='font-medium'>{product.name}</h4>
                                 <p className='mt-1'>&#8377;{product.price}</p></Link>
